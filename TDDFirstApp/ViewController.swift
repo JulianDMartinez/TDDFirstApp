c069d483f637cb094dca2a392f4c685b0092af35
Julian Martinez <julian.martinez.s@outlook.com>
2022-07-02 13:42:45 -0500

    Initial Commit

---
+++ TDDFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDDFirstApp
+//
+//  Created by Julian Martinez on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TDDFirstAppUITests/TDDFirstAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TDDFirstAppUITestsLaunchTests.swift
+//  TDDFirstAppUITests
+//
+//  Created by Julian Martinez on 7/2/22.
+//
+
+import XCTest
+
+class TDDFirstAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
