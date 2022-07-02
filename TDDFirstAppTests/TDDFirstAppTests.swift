9271fec009c9326c4a8b7eb4a0357b5ffd10247d
Julian Martinez <julian.martinez.s@outlook.com>
2022-07-02 14:22:21 -0500

    Adds function and unit test for function.

--- TDDFirstAppTests/TDDFirstAppTests.swift
+++ TDDFirstAppTests/TDDFirstAppTests.swift
@@ -18,19 +18,12 @@ class TDDFirstAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_numberOfVowels_whenGivenDominik_shouldReturn3() {
+        let viewController = ViewController()
+        
+        let result = viewController.numberOfVowels(in: "Dominik")
+        
+        XCTAssertEqual(result, 3, "Expected 3 vowels in 'Dominik' but got \(result)")
     }
 
 }
